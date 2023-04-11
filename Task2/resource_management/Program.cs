ce477d1f26cd57a6c978234b1e7514a829732a86
KirillFisenko <79265463469@ya.ru>
2023-04-11 18:18:05 +0300

    +

--- Task2/resource_management/Program.cs
+++ Task2/resource_management/Program.cs
@@ -1,116 +1,124 @@
 ﻿using System;
 using System.IO;
+using System.Text;
 
-public class TextFile : IDisposable
-{
-    public FileStream fileStream;
-    public StreamReader streamReader;
-    public StreamWriter streamWriter;
-
-    // Конструктор класса TextFile
-    public TextFile(string path, int length)
-    {
-        fileStream = new FileStream(path, FileMode.OpenOrCreate, FileAccess.ReadWrite, FileShare.ReadWrite);
-        streamReader = new StreamReader(fileStream);
-        streamWriter = new StreamWriter(fileStream);
-
-        fileStream.SetLength(length);
-        for (int i = 0; i < length; i++)
-        {
-            streamWriter.Write(' ');
-        }
-        streamWriter.Flush();
-    }
-
-    // Индексатор класса TextFile
-    public char this[int index]
-    {
-        get
-        {
-            fileStream.Seek(index, SeekOrigin.Begin);
-            return (char)streamReader.Read();
-        }
-        set
-        {
-            fileStream.Seek(index, SeekOrigin.Begin);
-            streamWriter.Write(value);
-            streamWriter.Flush();
-        }
-    }
-
-    // Свойство Length класса TextFile
-    public int Length
-    {
-        get { return (int)fileStream.Length; }
-    }
-
-    // Статический метод Create класса TextFile
-    public static TextFile Create(string path, int length)
-    {
-        return new TextFile(path, length);
-    }
-
-    // Метод Dispose класса TextFile
-    public void Dispose()
-    {
-        streamWriter.Close();
-        streamReader.Close();        
-        fileStream.Close();
-    }
-}
-
-public class Program
+class Program
 {
     static void Main(string[] args)
     {
-        TextFile textFile = null;
+        // Создание нового файла
+        var path = "test.txt";
+        var length = 15;
+        var encoding = Encoding.Default;
+        var file = MyFile.Create(path, length, encoding);
+
         try
         {
-            // Создание экземпляра класса TextFile
-            textFile = TextFile.Create("test.txt", 15);
-            textFile[0] = '[';
-            textFile[1] = '0';
-            textFile[2] = '1';
-            textFile[3] = ']';
-            textFile[4] = ' ';
-            textFile[5] = 'П';
-            textFile[6] = 'р';
-            textFile[7] = 'и';
-            textFile[8] = 'в';
-            textFile[9] = 'е';
-            textFile[10] = 'т';
-            textFile[11] = ' ';
-            textFile[12] = 'м';
-            textFile[13] = 'и';
-            textFile[14] = 'р';
+            // Сохранение в файл строки вида «[01] Привет мир!»
+            file[0] = '[';
+            file[1] = '0';
+            file[2] = '1';
+            file[3] = ']';
+            file[4] = ' ';
+            file[5] = 'П';
+            file[6] = 'р';
+            file[7] = 'и';
+            file[8] = 'в';
+            file[9] = 'е';
+            file[10] = 'т';
+            file[11] = ' ';
+            file[12] = 'м';
+            file[13] = 'и';
+            file[14] = 'р';
 
             Console.WriteLine("Содержимое файла после записи:");
-            for (int i = 0; i < textFile.Length; i++)
+            for (int i = 0; i < file.Length; i++)
             {
-                Console.Write(textFile[i]);
+                Console.Write(file[i]);
             }
             Console.WriteLine();
+
+            // Открытие созданного файла и изменение в нём одного символа, чтобы в файле получилось «[02] Привет мир!»
+            using (var reader = MyFile.Read(path, encoding))
+            {
+                reader[2] = '2';
+
+                Console.WriteLine("Содержимое файла после записи:");
+                for (int i = 0; i < reader.Length; i++)
+                {
+                    Console.Write(reader[i]);
+                }
+                Console.WriteLine();
+            }
         }
         finally
         {
             // Освобождение ресурсов
-            Console.WriteLine("Освобождение ресурсов...");
-            
-            textFile?.Dispose();
-        }
-
-        using (TextFile sw = new TextFile("test.txt", 15))
-        {
-            // Изменение содержимого файла
-            sw[1] = '2';
-
-            Console.WriteLine("Содержимое файла после изменения:");
-            for (int i = 0; i < sw.Length; i++)
-            {
-                Console.Write(sw[i]);
-            }
-            Console.WriteLine();
+            file.Dispose();
         }
         Console.ReadLine();
     }
+}
+
+class MyFile : IDisposable
+{
+    private FileStream _stream;
+    private StreamWriter _writer;
+    private StreamReader _reader;
+
+    public int Length { get; }
+
+    private MyFile(FileStream stream, int length, Encoding encoding)
+    {
+        _stream = stream;
+        _writer = new StreamWriter(_stream, encoding);
+        _reader = new StreamReader(_stream, encoding);
+        Length = length;
+    }
+
+    public static MyFile Create(string path, int length, Encoding encoding)
+    {
+        var stream = new FileStream(path, FileMode.Create, FileAccess.ReadWrite, FileShare.ReadWrite);
+        var writer = new StreamWriter(stream, encoding);
+        writer.Write(new string(' ', length));
+        writer.Flush();
+        return new MyFile(stream, length, encoding);
+    }
+
+    public static MyFile Read(string path, Encoding encoding)
+    {
+        var stream = new FileStream(path, FileMode.Open, FileAccess.ReadWrite, FileShare.ReadWrite);
+        var reader = new StreamReader(stream, encoding);
+        var length = (int)stream.Length;
+        return new MyFile(stream, length, encoding);
+    }
+
+    public char this[int index]
+    {
+        get
+        {
+            if (index < 0 || index >= Length)
+                throw new ArgumentOutOfRangeException(nameof(index));
+
+            _stream.Seek(index, SeekOrigin.Begin);
+            return (char)_reader.Read();
+        }
+        set
+        {
+            if (index < 0 || index >= Length)
+                throw new ArgumentOutOfRangeException(nameof(index));
+
+            _stream.Seek(index, SeekOrigin.Begin);
+            _writer.Write(value);
+            _writer.Flush();
+        }
+    }
+
+    public void Dispose()
+    {
+        _writer.Dispose();
+        _reader.Dispose();
+        _stream.Dispose();
+    }
+
 }
