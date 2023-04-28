3ba078d2fa6dc0ca03ddec4e9d5c4d7e8d7102af
KirillFisenko <79265463469@ya.ru>
2023-04-28 18:30:42 +0300

    +

--- Task6/Task6/LINQ/Program.cs
+++ Task6/Task6/LINQ/Program.cs
@@ -1,5 +1,4 @@
 ﻿using System;
-using System.Collections.Generic;
 using System.Diagnostics;
 using System.Linq;
 
@@ -10,7 +9,7 @@ namespace LINQ
         // расширяющий метод, который определяет сумму элементов массива
         public static int Sum(this int[] array)
         {
-            var sum = 0;
+            int sum = 0;
             foreach (int number in array)
             {
                 sum += number;
@@ -25,37 +24,66 @@ namespace LINQ
             {
                 return false;
             }
-
-            foreach (var symbol in str)
+            foreach (char symbol in str)
             {
                 if (!char.IsDigit(symbol))
                 {
                     return false;
                 }
             }
-
             return true;
         }
 
         // поиск всех положительных элементов в массиве
-        public static IEnumerable<int> FindPositiveNumbers(this int[] array)
+        public static int[] FindPositiveNumbers1(this int[] numbers)
         {
-            return from number in array
-                   where number > 0
-                   select number;
-        }
+            int count = 0;
+            for (int i = 0; i < numbers.Length; i++)
+            {
+                if (numbers[i] > 0)
+                {
+                    count++;
+                }
+            }
+            int[] result = new int[count];
+            int index = 0;
+            for (int i = 0; i < numbers.Length; i++)
+            {
+                if (numbers[i] > 0)
+                {
+                    result[index] = numbers[i];
+                    index++;
+                }
+            }
+            return result;
+        }        
 
-        public static IEnumerable<int> FindNumbers(this int[] array, Func<int, bool> predicate)
+        public static int[] FindPositiveNumbers2(this int[] numbers, Func<int, bool> condition)
         {
-            return from number in array
-                   where predicate(number)
-                   select number;
+            int count = 0;
+            for (int i = 0; i < numbers.Length; i++)
+            {
+                if (condition(numbers[i]))
+                {
+                    count++;
+                }
+            }
+            int[] result = new int[count];
+            int index = 0;
+            for (int i = 0; i < numbers.Length; i++)
+            {
+                if (condition(numbers[i]))
+                {
+                    result[index] = numbers[i];
+                    index++;
+                }
+            }
+            return result;
         }
     }
 
-    internal class Program
-    {
-        public static Random random = new Random();
+    public class Program
+    {        
         static void Main(string[] args)
         {
             // Задание 1
@@ -74,40 +102,39 @@ namespace LINQ
 
             // Задание 3
             // Создаем большой массив
-            int[] bigArray = new int[100000];
+            int[] bigArray = new int[1000000];
 
             // Заполнение массива случайными числами
-            
+            Random random = new Random();
             for (int i = 0; i < bigArray.Length; i++)
             {
                 bigArray[i] = random.Next(-1000000, 1000000);
             }
 
-            var count = 10; //количество измерений
+            int count = 100; //количество измерений
 
             // Измерение времени выполнения метода FindPositiveNumbers()
-            var measurements1 = new int[count];
-            
+            long[] measurements1 = new long[count];            
             for (int i = 0; i < measurements1.Length; i++)
             {
                 Stopwatch stopwatch1 = new Stopwatch();                
                 stopwatch1.Start();
-                var numbers1 = bigArray.FindPositiveNumbers();
+                var numbers1 = bigArray.FindPositiveNumbers1();                
                 stopwatch1.Stop();
-                measurements1[i] = (int)stopwatch1.ElapsedMilliseconds;                
+                measurements1[i] = stopwatch1.ElapsedMilliseconds;                              
             }
             Array.Sort(measurements1);
-            var measurements1Middle = measurements1[measurements1.Length / 2];
+            long measurements1Middle = measurements1[measurements1.Length / 2];
             Console.WriteLine($"Время выполнения метода FindPositiveNumbers(): {measurements1Middle} мс");
             Console.WriteLine();
 
-            // Измерение времени выполнения метода FindNumbers() с использованием делегата
+            // Измерение времени выполнения метода FindPositiveNumbers() с использованием делегата
             var measurements2 = new int[count];
             for (int i = 0; i < measurements2.Length; i++)
             {
                 Stopwatch stopwatch2 = new Stopwatch();                
                 stopwatch2.Start();
-                var numbers2 = bigArray.FindNumbers(delegate (int x) { return x > 0; });
+                var numbers2 = bigArray.FindPositiveNumbers2(delegate (int x) { return x > 0; });
                 stopwatch2.Stop();
                 measurements2[i] = (int)stopwatch2.ElapsedMilliseconds;                
             }
@@ -116,13 +143,13 @@ namespace LINQ
             Console.WriteLine($"Время выполнения метода FindPositiveNumbers() с использованием делегата: {measurements2Middle} мс");
             Console.WriteLine();
 
-            // Измерение времени выполнения метода FindNumbers() с использованием анонимного метода            
+            // Измерение времени выполнения метода FindPositiveNumbers() с использованием анонимного метода            
             var measurements3 = new int[count];
             for (int i = 0; i < measurements3.Length; i++)
             {
                 Stopwatch stopwatch3 = new Stopwatch();                
                 stopwatch3.Start();
-                var numbers3 = bigArray.FindNumbers((x) => x > 0);
+                var numbers3 = bigArray.FindPositiveNumbers2((x) => x > 0);
                 stopwatch3.Stop();
                 measurements3[i] = (int)stopwatch3.ElapsedMilliseconds;                
             }
@@ -131,19 +158,34 @@ namespace LINQ
             Console.WriteLine($"Время выполнения метода FindPositiveNumbers() с использованием анонимного метода: {measurements3Middle} мс");
             Console.WriteLine();
 
-            // Измерение времени выполнения метода FindNumbers() с использованием лямбда-выражения
+            // Измерение времени выполнения метода FindPositiveNumbers() с использованием лямбда-выражения
             var measurements4 = new int[count];
             for (int i = 0; i < measurements4.Length; i++)
             {
                 Stopwatch stopwatch4 = new Stopwatch();                
                 stopwatch4.Start();
-                var numbers4 = bigArray.FindNumbers(x => x > 0);
+                var numbers4 = bigArray.FindPositiveNumbers2(x => x > 0);
                 stopwatch4.Stop();
                 measurements4[i] = (int)stopwatch4.ElapsedMilliseconds;                
             }
             Array.Sort(measurements4);
             var measurements4Middle = measurements4[measurements4.Length / 2];
             Console.WriteLine($"Время выполнения метода FindPositiveNumbers() с использованием лямбда-выражения: {measurements4Middle} мс");
+            Console.WriteLine();
+
+            // Измерение времени выполнения метода FindPositiveNumbers() с использованием LINQ
+            var measurements5 = new int[count];
+            for (int i = 0; i < measurements5.Length; i++)
+            {
+                Stopwatch stopwatch5 = new Stopwatch();
+                stopwatch5.Start();
+                var numbers5 = bigArray.Where(n => n > 0).ToArray();
+                stopwatch5.Stop();
+                measurements5[i] = (int)stopwatch5.ElapsedMilliseconds;
+            }
+            Array.Sort(measurements5);
+            var measurements5Middle = measurements5[measurements5.Length / 2];
+            Console.WriteLine($"Время выполнения метода FindPositiveNumbers() с использованием LINQ: {measurements5Middle} мс");
             Console.WriteLine();
 
             Console.ReadLine();
