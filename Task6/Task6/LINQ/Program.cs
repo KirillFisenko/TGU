9ac1cde6b4207611cd967daae12a15914db74dce
KirillFisenko <79265463469@ya.ru>
2023-04-28 17:28:45 +0300

    +

--- Task6/Task6/LINQ/Program.cs
+++ Task6/Task6/LINQ/Program.cs
@@ -7,9 +7,10 @@ namespace LINQ
 {
     public static class Extensions
     {
+        // расширяющий метод, который определяет сумму элементов массива
         public static int Sum(this int[] array)
         {
-            int sum = 0;
+            var sum = 0;
             foreach (int number in array)
             {
                 sum += number;
@@ -17,18 +18,26 @@ namespace LINQ
             return sum;
         }
 
-        public static bool IsPositiveInteger(this string str)
+        // расширяющий метод, который определяет, является ли строка положительным целым числом
+        public static bool IsPositiveInt(this string str)
         {
             if (string.IsNullOrEmpty(str))
+            {
                 return false;
+            }
 
-            foreach (char c in str)
-                if (!char.IsDigit(c))
+            foreach (var symbol in str)
+            {
+                if (!char.IsDigit(symbol))
+                {
                     return false;
+                }
+            }
 
             return true;
         }
 
+        // поиск всех положительных элементов в массиве
         public static IEnumerable<int> FindPositiveNumbers(this int[] array)
         {
             return from number in array
@@ -44,208 +53,99 @@ namespace LINQ
         }
     }
 
-    //public static class ArrayExtensions
-    //{
-    //    // расширяющий метод, который определяет сумму элементов массива
-    //    public static int Sum(this int[] array)
-    //    {
-    //        int sum = 0;
-    //        for (int i = 0; i < array.Length; i++)
-    //        {
-    //            sum += array[i];
-    //        }
-    //        return sum;
-    //    }
-
-    //    // методы поиска элемента в массиве
-    //    public static IEnumerable<int> FindPositiveNumbers(this int[] array)
-    //    {
-    //        for (int i = 0; i < array.Length; i++)
-    //        {
-    //            if (array[i] > 0)
-    //            {
-    //                yield return array[i];
-    //            }
-    //        }
-    //    }
-
-    //    public static IEnumerable<int> FindNumbers(this int[] array, Func<int, bool> predicate)
-    //    {
-    //        for (int i = 0; i < array.Length; i++)
-    //        {
-    //            if (predicate(array[i]))
-    //            {
-    //                yield return array[i];
-    //            }
-    //        }
-    //    }
-
-    //    public static IEnumerable<int> FindNumbers1(this int[] array, Func<int, bool> predicate)
-    //    {
-    //        for (int i = 0; i < array.Length; i++)
-    //        {
-    //            if (predicate(array[i]))
-    //            {
-    //                yield return array[i];
-    //            }
-    //        }
-    //    }
-
-    //    public static IEnumerable<int> FindNumbers2(this int[] array, Func<int, bool> predicate)
-    //    {
-    //        for (int i = 0; i < array.Length; i++)
-    //        {
-    //            if (predicate(array[i]))
-    //            {
-    //                yield return array[i];
-    //            }
-    //        }
-    //    }
-
-    //    public static IEnumerable<int> FindNumbers3(this int[] array, Func<int, bool> predicate)
-    //    {
-    //        return array.Where(predicate);
-    //    }
-
-    //}
-
-
-    //// расширяющий метод, который определяет, является ли строка положительным целым числом
-    //public static class StringExtensions
-    //{
-    //    public static bool IsPositiveInteger(this string str)
-    //    {
-    //        if (string.IsNullOrEmpty(str))
-    //        {
-    //            return false;
-    //        }
-
-    //        foreach (char c in str)
-    //        {
-    //            if (!char.IsDigit(c))
-    //            {
-    //                return false;
-    //            }
-    //        }
-
-    //        return true;
-    //    }
-    //}
-
-
     internal class Program
     {
+        public static Random random = new Random();
         static void Main(string[] args)
         {
             // Задание 1
-            int[] array = { 1, 2, 3, 4, 5 };
-            int sum = array.Sum();
-            Console.WriteLine($"Сумма элементов массива: {sum}");
+            // Определяем сумму элементов массива расширяющим методом
+            int[] array1 = { 1, 2, 3, 4, 5 };
+            int sum = array1.Sum();
+            Console.WriteLine($"Сумма элементов массива {array1}: {sum}");
+            Console.WriteLine();
 
             // Задание 2
+            // Определяем, является ли строка целым положительным числом
             string str = "123";
-            bool isPositiveInteger = str.IsPositiveInteger();
-            Console.WriteLine($"Строка является положительным целым числом: {isPositiveInteger}");
+            bool isPositiveInt = str.IsPositiveInt();
+            Console.WriteLine($"Строка {str} является положительным целым числом: {isPositiveInt}");
+            Console.WriteLine();
 
             // Задание 3
-            int[] array2 = { -1, 2, -3, 4, -5 };
-            IEnumerable<int> positiveNumbers = array2.FindPositiveNumbers();
-            Console.WriteLine("Положительные числа в массиве:");
-            foreach (int number in positiveNumbers)
-            {
-                Console.WriteLine(number);
-            }
-
-            IEnumerable<int> numbers = array2.FindNumbers(x => x > 0);
-            Console.WriteLine("Числа больше нуля в массиве:");
-            foreach (int number in numbers)
-            {
-                Console.WriteLine(number);
-            }
-
-            IEnumerable<int> numbers2 = array2.FindNumbers(delegate (int x) { return x > 0; });
-            Console.WriteLine("Числа больше нуля в массиве:");
-            foreach (int number in numbers2)
-            {
-                Console.WriteLine(number);
-            }
-
-            IEnumerable<int> numbers3 = array2.FindNumbers((x) => x > 0);
-            Console.WriteLine("Числа больше нуля в массиве:");
-            foreach (int number in numbers3)
-            {
-                Console.WriteLine(number);
-            }
-
-            IEnumerable<int> numbers4 = array2.FindNumbers(x => x > 0);
-            Console.WriteLine("Числа больше нуля в массиве:");
-            foreach (int number in numbers4)
-            {
-                Console.WriteLine(number);
-            }
-
-            IEnumerable<int> numbers5 = from number in array2
-                                        where number > 0
-                                        select number;
-            Console.WriteLine("Числа больше нуля в массиве:");
-            foreach (int number in numbers5)
-            {
-                Console.WriteLine(number);
-            }
-
-            // Сравнение скорости выполнения вычислений
+            // Создаем большой массив
             int[] bigArray = new int[100000];
 
             // Заполнение массива случайными числами
-            Random random = new Random();
+            
             for (int i = 0; i < bigArray.Length; i++)
             {
                 bigArray[i] = random.Next(-1000000, 1000000);
             }
 
+            var count = 10; //количество измерений
+
             // Измерение времени выполнения метода FindPositiveNumbers()
-            Stopwatch stopwatch1 = new Stopwatch();
-            stopwatch1.Start();
-            for (int i = 0; i < 100; i++)
+            var measurements1 = new int[count];
+            
+            for (int i = 0; i < measurements1.Length; i++)
             {
-                IEnumerable<int> positiveNumbers2 = bigArray.FindPositiveNumbers();
-                foreach (int number in positiveNumbers2) { }
+                Stopwatch stopwatch1 = new Stopwatch();                
+                stopwatch1.Start();
+                var numbers1 = bigArray.FindPositiveNumbers();
+                stopwatch1.Stop();
+                measurements1[i] = (int)stopwatch1.ElapsedMilliseconds;                
             }
-            stopwatch1.Stop();
-            Console.WriteLine($"Время выполнения метода FindPositiveNumbers(): {stopwatch1.ElapsedMilliseconds} мс");
+            Array.Sort(measurements1);
+            var measurements1Middle = measurements1[measurements1.Length / 2];
+            Console.WriteLine($"Время выполнения метода FindPositiveNumbers(): {measurements1Middle} мс");
+            Console.WriteLine();
 
             // Измерение времени выполнения метода FindNumbers() с использованием делегата
-            Stopwatch stopwatch2 = new Stopwatch();
-            stopwatch2.Start();
-            for (int i = 0; i < 100; i++)
+            var measurements2 = new int[count];
+            for (int i = 0; i < measurements2.Length; i++)
             {
-                IEnumerable<int> numbers6 = bigArray.FindNumbers(delegate (int x) { return x > 0; });
-                foreach (int number in numbers6) { }
+                Stopwatch stopwatch2 = new Stopwatch();                
+                stopwatch2.Start();
+                var numbers2 = bigArray.FindNumbers(delegate (int x) { return x > 0; });
+                stopwatch2.Stop();
+                measurements2[i] = (int)stopwatch2.ElapsedMilliseconds;                
             }
-            stopwatch2.Stop();
-            Console.WriteLine($"Время выполнения метода FindNumbers() с использованием делегата: {stopwatch2.ElapsedMilliseconds} мс");
+            Array.Sort(measurements2);
+            var measurements2Middle = measurements2[measurements2.Length / 2];
+            Console.WriteLine($"Время выполнения метода FindPositiveNumbers() с использованием делегата: {measurements2Middle} мс");
+            Console.WriteLine();
 
-            // Измерение времени выполнения метода FindNumbers() с использованием анонимного метода
-            Stopwatch stopwatch3 = new Stopwatch();
-            stopwatch3.Start();
-            for (int i = 0; i < 100; i++)
+            // Измерение времени выполнения метода FindNumbers() с использованием анонимного метода            
+            var measurements3 = new int[count];
+            for (int i = 0; i < measurements3.Length; i++)
             {
-                IEnumerable<int> numbers7 = bigArray.FindNumbers((x) => x > 0);
-                foreach (int number in numbers7) { }
+                Stopwatch stopwatch3 = new Stopwatch();                
+                stopwatch3.Start();
+                var numbers3 = bigArray.FindNumbers((x) => x > 0);
+                stopwatch3.Stop();
+                measurements3[i] = (int)stopwatch3.ElapsedMilliseconds;                
             }
-            stopwatch3.Stop();
-            Console.WriteLine($"Время выполнения метода FindNumbers() с использованием анонимного метода: {stopwatch3.ElapsedMilliseconds} мс");
+            Array.Sort(measurements3);
+            var measurements3Middle = measurements3[measurements3.Length / 2];
+            Console.WriteLine($"Время выполнения метода FindPositiveNumbers() с использованием анонимного метода: {measurements3Middle} мс");
+            Console.WriteLine();
 
             // Измерение времени выполнения метода FindNumbers() с использованием лямбда-выражения
-            Stopwatch stopwatch4 = new Stopwatch();
-            stopwatch4.Start();
-            for (int i = 0; i < 100; i++)
+            var measurements4 = new int[count];
+            for (int i = 0; i < measurements4.Length; i++)
             {
-                IEnumerable<int> numbers8 = bigArray.FindNumbers(x => x > 0);
-                foreach (int number in numbers8) { }
+                Stopwatch stopwatch4 = new Stopwatch();                
+                stopwatch4.Start();
+                var numbers4 = bigArray.FindNumbers(x => x > 0);
+                stopwatch4.Stop();
+                measurements4[i] = (int)stopwatch4.ElapsedMilliseconds;                
             }
-            stopwatch4.Stop();
-            Console.WriteLine($"Время выполнения метода FindNumbers() с использованием лямбда-выражения: {stopwatch4.ElapsedMilliseconds} мс");
+            Array.Sort(measurements4);
+            var measurements4Middle = measurements4[measurements4.Length / 2];
+            Console.WriteLine($"Время выполнения метода FindPositiveNumbers() с использованием лямбда-выражения: {measurements4Middle} мс");
+            Console.WriteLine();
+
             Console.ReadLine();
         }
     }
